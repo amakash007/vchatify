9af762071510aeadb92d6a73179acf0dc0874525
Akash Dhage <dhageakash2004@gmail.com>
2025-12-21 12:10:22 +0530

    Update code

--- frontend/src/components/FriendCard.jsx
+++ frontend/src/components/FriendCard.jsx
@@ -25,7 +25,7 @@ const FriendCard = ({ friend }) => {
         </div>
 
         <Link to={`/chat/${friend._id}`} className="btn btn-outline w-full">
-          Message
+          Messages
         </Link>
       </div>
     </div>
